223d4cbacdcc6cb1bc915479a4b93d11417f4429
Sultan Iman <sultan.imanhodjaev@gmail.com>
2025-05-12 21:07:18 +0200

    use go-cmp instead of testify

--- env_test.go
+++ env_test.go
@@ -1,9 +1,10 @@
 package env
 
 import (
+	"runtime"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/google/go-cmp/cmp"
 )
 
 const (
@@ -11,34 +12,52 @@ const (
 	secondVar = "SECRET_INT"
 )
 
+const failMessage = `
+Wanted: %v
+Got: %v
+Test case: %s::%s:%d
+`
+
+func assertEqual(t *testing.T, expected any, having any) {
+	if ok := cmp.Equal(expected, having); !ok {
+		_, f, l, _ := runtime.Caller(1)
+		t.Errorf(failMessage, expected, having, f, t.Name(), l)
+		t.Fail()
+	}
+}
+
+func assertError(t *testing.T, err error) {
+	if err == nil {
+		_, f, l, _ := runtime.Caller(1)
+		t.Errorf(failMessage, "error", err, f, t.Name(), l)
+		t.Fail()
+	}
+}
+
 func TestGetInt(t *testing.T) {
 	t.Setenv(firstVar, "1")
 	t.Setenv(secondVar, "1")
 
 	someInt := GetInt(firstVar)
-	assert.Equal(t, 1, someInt)
+	assertEqual(t, 1, someInt)
 
 	otherInt := GetInt(secondVar)
-	assert.Equal(t, 1, otherInt)
+	assertEqual(t, 1, otherInt)
 
 	randomInt := GetInt("OTHER_INTS")
-	assert.Equal(t, 0, randomInt)
+	assertEqual(t, 0, randomInt)
 
 	someInt8 := GetInt8(firstVar)
-	assert.IsType(t, int8(0), someInt8)
-	assert.Equal(t, int8(1), someInt8)
+	assertEqual(t, int8(1), someInt8)
 
 	someInt16 := GetInt16(firstVar)
-	assert.IsType(t, int16(0), someInt16)
-	assert.Equal(t, int16(1), someInt16)
+	assertEqual(t, int16(1), someInt16)
 
 	someInt32 := GetInt32(firstVar)
-	assert.IsType(t, int32(0), someInt32)
-	assert.Equal(t, int32(1), someInt32)
+	assertEqual(t, int32(1), someInt32)
 
 	someInt64 := GetInt64(firstVar)
-	assert.IsType(t, int64(0), someInt64)
-	assert.Equal(t, int64(1), someInt64)
+	assertEqual(t, int64(1), someInt64)
 }
 
 func TestGetUInt(t *testing.T) {
@@ -46,29 +65,25 @@ func TestGetUInt(t *testing.T) {
 	t.Setenv(secondVar, "101")
 
 	someUInt := GetUInt(firstVar)
-	assert.Equal(t, uint(111), someUInt)
+	assertEqual(t, uint(111), someUInt)
 
 	otherUInt := GetUInt(secondVar)
-	assert.Equal(t, uint(101), otherUInt)
+	assertEqual(t, uint(101), otherUInt)
 
 	randomInt := GetUInt("OTHER_INTS")
-	assert.Equal(t, uint(0), randomInt)
+	assertEqual(t, uint(0), randomInt)
 
 	someInt8 := GetUInt8(firstVar)
-	assert.IsType(t, uint8(0), someInt8)
-	assert.Equal(t, uint8(111), someInt8)
+	assertEqual(t, uint8(111), someInt8)
 
 	someInt16 := GetUInt16(firstVar)
-	assert.IsType(t, uint16(0), someInt16)
-	assert.Equal(t, uint16(111), someInt16)
+	assertEqual(t, uint16(111), someInt16)
 
 	someInt32 := GetUInt32(firstVar)
-	assert.IsType(t, uint32(0), someInt32)
-	assert.Equal(t, uint32(111), someInt32)
+	assertEqual(t, uint32(111), someInt32)
 
 	someInt64 := GetUInt64(firstVar)
-	assert.IsType(t, uint64(0), someInt64)
-	assert.Equal(t, uint64(111), someInt64)
+	assertEqual(t, uint64(111), someInt64)
 }
 
 func TestGetFloat(t *testing.T) {
@@ -76,16 +91,16 @@ func TestGetFloat(t *testing.T) {
 	t.Setenv(secondVar, "101.0012")
 
 	someFloat32 := GetFloat32(firstVar)
-	assert.Equal(t, float32(111.001), someFloat32)
+	assertEqual(t, float32(111.001), someFloat32)
 
 	otherFloat64 := GetFloat64(secondVar)
-	assert.Equal(t, float64(101.0012), otherFloat64)
+	assertEqual(t, float64(101.0012), otherFloat64)
 
 	randomFloat32 := GetFloat32("RANDOM_FLOAT")
-	assert.Equal(t, float32(0.0), randomFloat32)
+	assertEqual(t, float32(0.0), randomFloat32)
 
 	randomFloat64 := GetFloat64("RANDOM_FLOAT")
-	assert.Equal(t, float64(0.0), randomFloat64)
+	assertEqual(t, float64(0.0), randomFloat64)
 }
 
 func TestGetBool(t *testing.T) {
@@ -93,18 +108,31 @@ func TestGetBool(t *testing.T) {
 	t.Setenv(secondVar, "false")
 
 	firstBool := GetBool(firstVar)
-	assert.Equal(t, true, firstBool)
+	assertEqual(t, true, firstBool)
 
 	secondBool := GetBool(secondVar)
-	assert.Equal(t, false, secondBool)
+	assertEqual(t, false, secondBool)
 
 	randomBool := GetBool("random")
-	assert.Equal(t, false, randomBool)
+	assertEqual(t, false, randomBool)
 }
 
+func TestGetBoolE(t *testing.T) {
+	t.Setenv(firstVar, "TTrue")
+	t.Setenv(secondVar, "FFalse")
+
+	firstBool, err := GetBoolE(firstVar)
+	assertEqual(t, false, firstBool)
+	assertError(t, err)
+
+	secondBool, err := GetBoolE(secondVar)
+	assertEqual(t, false, secondBool)
+	assertError(t, err)
+
+}
 func TestGetWithPrefix(t *testing.T) {
 	SetEnvPrefix("PREFIX_")
 	t.Setenv("PREFIX_"+firstVar, "123")
 	firstInt := GetInt(firstVar)
-	assert.Equal(t, 123, firstInt)
+	assertEqual(t, 123, firstInt)
 }
